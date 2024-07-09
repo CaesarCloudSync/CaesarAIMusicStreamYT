d1c3b511697eae9674f82155b6f9534912b227ee
amari <amari.lawal@gmail.com>
2024-07-09 18:45:35 +0100

    using ytdlp command line instead of pytube

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "google_cloud_run_service" "run_service" {
   template {
     spec {
       containers {
-        image = "palondomus/caesaraimusicstreamyt:16"
+        image = "palondomus/caesaraimusicstreamyt:17"
       }
     }
   }
