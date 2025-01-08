71f5bdae2e85581c60d38c3765a01ac47295c440
Amari <amari.lawal@gmail.com>
2025-01-08 18:42:15 +0000

    detacthed background raspberry pi

--- run_app.sh
+++ run_app.sh
@@ -3,4 +3,4 @@ image="caesaraimusicstreamyt"
 latest="latest"
 # Test application
 docker build -t palondomus/$image:$latest .
-docker run -it -p 8080:8080 palondomus/$image:$latest
+docker run -d -p 8080:8080 palondomus/$image:$latest # -it
