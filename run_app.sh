e9ccfa4b76de0f3419116ae97f20a1c0e16d6236
Amari <amari.lawal@gmail.com>
2025-01-08 19:12:41 +0000

    allow restart of docker containerr on reboot

--- run_app.sh
+++ run_app.sh
@@ -3,4 +3,4 @@ image="caesaraimusicstreamyt"
 latest="latest"
 # Test application
 docker build -t palondomus/$image:$latest .
-docker run -d -p 8080:8080 palondomus/$image:$latest # -it
+docker run -d --restart always -p 8080:8080 palondomus/$image:$latest # -it
