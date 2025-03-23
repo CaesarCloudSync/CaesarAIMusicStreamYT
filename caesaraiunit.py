0f75bd980fa026c9313bb98b8ab2da20c8204161
amari <amari.lawal@gmail.com>
2025-03-23 22:04:34 +0000

    all services work perfecrtly. movies, music and it is accessible from the internet

--- caesaraiunit.py
+++ caesaraiunit.py
@@ -3,11 +3,12 @@ import requests
 import unittest
 import sys
 
-uri = "http://127.0.0.1:8080" #"https://blacktechdivisionreward-hrjw5cc7pa-uc.a.run.app"
+base_url = "https://hub.caesaraihub.org" 
+
 
 class CaesarAIUnittest(unittest.TestCase):
     def test_get_audio(self):
-        response = requests.get(f"{uri}/getaudio",params={"url":"https://www.youtube.com/watch?v=sSudJNPsxAc"})
+        response = requests.get(f"{base_url}/caesaraimusicstreamyt/8080/getaudio",params={"url":"https://www.youtube.com/watch?v=sSudJNPsxAc"})
         print(response.json())
 
 if __name__ == "__main__":
