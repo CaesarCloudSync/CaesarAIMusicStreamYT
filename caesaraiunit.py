0ce72e1acc3fd23a02ab0898d9368dd0ef3ece26
amari <amari.lawal@gmail.com>
2025-03-25 16:24:04 +0000

    change unittests

--- caesaraiunit.py
+++ caesaraiunit.py
@@ -3,12 +3,12 @@ import requests
 import unittest
 import sys
 
-base_url = "https://hub.caesaraihub.org" 
+base_url = "https://music.caesaraihub.org/" 
 
 
 class CaesarAIUnittest(unittest.TestCase):
     def test_get_audio(self):
-        response = requests.get(f"{base_url}/caesaraimusicstreamyt/8080/getaudio",params={"url":"https://www.youtube.com/watch?v=sSudJNPsxAc"})
+        response = requests.get(f"{base_url}/getaudio",params={"url":"https://www.youtube.com/watch?v=sSudJNPsxAc"})
         print(response.json())
 
 if __name__ == "__main__":
